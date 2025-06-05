028b6a7193a824ac36e9f8f2cd416d72325b4502
AtharvaBOT7 <atharvachundurwar841@gmail.com>
2025-06-04 23:03:47 -0700

    completed modular code added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'QApplication',
+    version = '0.0.1',
+    author = 'Atharva Chundurwar',
+    author_email = 'atharavchundurwar841@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
